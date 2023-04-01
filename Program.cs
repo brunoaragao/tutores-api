fe06f98e771ffe22a93d6d18d38dec3d343fb637
brunoaragao <bruno20aragao@gmail.com>
2023-04-01 12:21:08 -0300

    cria api

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TutorContext>(options =>
+    options.UseInMemoryDatabase("TutoresInMemory"));
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+var tutores = app.MapGroup("/tutores");
+
+tutores.MapGet("/", async (TutorContext context)
+    => await context.Tutores.ToListAsync());
+
+tutores.MapGet("/{id}", async (int id, TutorContext context)
+    => await context.Tutores.FindAsync(id));
+
+tutores.MapPost("/", async (Tutor tutor, TutorContext context) =>
+{
+    context.Add(tutor);
+    await context.SaveChangesAsync();
+});
+
+tutores.MapPut("/", async (Tutor tutor, TutorContext context) =>
+{
+    context.Update(tutor);
+    await context.SaveChangesAsync();
+});
+
+tutores.MapDelete("/{id}", async (int id, TutorContext context) =>
+{
+    var tutor = await context.Tutores.FindAsync(id);
+
+    if (tutor is null) return;
+
+    context.Remove(tutor);
+    await context.SaveChangesAsync();
+});
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
+
+public record Tutor(int Id, string Name, string Email, string Senha);
+
+public class TutorContext : DbContext
+{
+    public TutorContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Tutor> Tutores => Set<Tutor>();
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Tutores API
+Api para o CRUD de tutores.
+
+## Sobre
+Esta API foi desenvolvida para o [Desafio Back-end 6](https://www.alura.com.br/challenges/back-end-6) da [Alura](https://www.alura.com.br/).
+
+## Pré-requisitos
+- [Docker](https://docs.docker.com/install/)
+
+## Como executar
+- Use o comando abaixo para executar a aplicação:
+```bash
+docker run -p 5000:80 brunoaragao/tutores-api
+```
+- Acesse a documentação da API em [http://localhost:5000/swagger](http://localhost:5000/swagger)
+
+## Tecnologias utilizadas
+- [.NET 7](https://dotnet.microsoft.com/download/dotnet/7.0)
+- [Docker](https://www.docker.com/)
